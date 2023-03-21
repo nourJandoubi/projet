f620b9b92eae9b9b504334efc063cac6abf77cfb
DorraBenMimoun <dorrabenmimoun@gmail.com>
2023-03-21 13:51:53 +0100

    test2

--- BourseForYou (1)/src/app/components/home/home.component.ts
+++ BourseForYou (1)/src/app/components/home/home.component.ts
@@ -16,42 +16,76 @@ export class HomeComponent implements OnInit {
     .map((x, i) => String.fromCharCode(i + 65));
   actualites: Actualite[] = [];
   actions: Action[] = [];
-  pageSlice: Actualite[] = [];
-  showText = false;
-  searchTerm="";
-  filteredItems: Actualite[] = [];
+  pageSliceNews: Actualite[] = [];//pageSliceNews des actualites
+  searchTermNews="";//search term des actualites
+  filteredItemsNews: Actualite[] = []; //filtred items des actualites apres utiliser le search term
 
+  pageSliceAction: Action[] = [];//pageSliceNews des actualites
+  searchTermAction="";//search term des actualites
+  filteredItemsAction: Action[] = [];
 
-
-  
-  onSearchTermChange(event: any) {
-    this.searchTerm = (event.target as HTMLInputElement).value;     
-      let term = this.searchTerm ? this.searchTerm.toLowerCase() : '';
+  //filtrage
+  onsearchTermNewsChange(event: any) {
+    this.searchTermNews = (event.target as HTMLInputElement).value;     
+      let term = this.searchTermNews ? this.searchTermNews.toLowerCase() : '';
     
       if (term.trim() !== '') {
-        this.filteredItems = this.actualites.filter(item => {
+        this.filteredItemsNews = this.actualites.filter(item => {
           if (typeof item.title === 'string') {
-            this.pageSlice= this.filteredItems.slice(0,3);
+            this.pageSliceNews= this.filteredItemsNews.slice(0,3);
             return item.title.toLowerCase().includes(term);
            
           }
           return false;
         });
       }
+      this.pageSliceNews=this.filteredItemsNews;
+      this.pageSliceNews= this.pageSliceNews.slice(0,4);
+
     
     
   }
+  onsearchTermActionChange(event: any) {
+    this.searchTermAction = (event.target as HTMLInputElement).value;     
+      let term = this.searchTermAction ? this.searchTermAction.toLowerCase() : '';
+    console.log("term",term)
+      if (term.trim() !== '') {
+        this.filteredItemsAction = this.actions.filter(item => {
+          if (typeof item.nomEntreprise === 'string') {
+            this.pageSliceAction= this.filteredItemsAction.slice(0,8);
+            return item.nomEntreprise.toLowerCase().includes(term);
+           
+          }
+          return false;
+        });
+      }
+      this.pageSliceAction=this.filteredItemsAction;
+      this.pageSliceAction= this.pageSliceAction.slice(0,8);
+    
+  }
 
-  onPageChange(event) {
+
+
+  //Pagination
+  onPageChangeNews(event) {
   
     const startIndex = event.pageIndex * event.pageSize;
     let endIndex = startIndex + event.pageSize;
-    if (endIndex > this.filteredItems.length) {
-      endIndex = this.filteredItems.length;
+    if (endIndex > this.filteredItemsNews.length) {
+      endIndex = this.filteredItemsNews.length;
     }
-    this.pageSlice = this.filteredItems.slice(startIndex, endIndex);
+    this.pageSliceNews = this.filteredItemsNews.slice(startIndex, endIndex);
   }
-
+  onPageChangeAction(event) {
+  
+    const startIndex = event.pageIndex * event.pageSize;
+    let endIndex = startIndex + event.pageSize;
+    if (endIndex > this.filteredItemsAction.length) 
+    {
+      endIndex = this.filteredItemsAction.length;
+    }
+    this.pageSliceAction= this.filteredItemsAction.slice(startIndex, endIndex);
+  }
 
 
 
@@ -60,8 +94,8 @@ export class HomeComponent implements OnInit {
     setTimeout(() => {
       this.actualiteService.getActualites().subscribe(data => {
         this.actualites = data;
-        this.filteredItems=data;
-        this.pageSlice= data.slice(0,3);
+        this.filteredItemsNews=data;
+        this.pageSliceNews= data.slice(0,4);
 
    
         for(let i=0; i<this.actualites.length; i++) {
@@ -75,6 +109,8 @@ export class HomeComponent implements OnInit {
     setTimeout(() => {
       this.actionService.getActualites().subscribe(data => {
         this.actions = data;
+        this.filteredItemsAction=data;
+        this.pageSliceAction= data.slice(0,8);
         console.log(data);
       });
     }, 2000);
