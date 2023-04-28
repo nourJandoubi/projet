491b1df4a1f45f3d6cf917440e0cd46e376e6221
nour jandoubi <jandoubi.nourelhouda@gmail.com>
2023-04-28 15:26:38 +0100

    'action'

--- BourseForYou (1)/src/app/components/actions/actions.component.ts
+++ BourseForYou (1)/src/app/components/actions/actions.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-
-import { ActionService } from 'src/app/services/action.service';
 import { Action } from 'src/app/models/action';
-
+import { ActionService } from 'src/app/services/action.service';
 @Component({
   selector: 'app-actions',
   templateUrl: './actions.component.html',
@@ -15,115 +13,40 @@ export class ActionsComponent {
     private route: ActivatedRoute,
     private router: Router
   ) {}
-
-  actions: Action[] = [];
-  location: string;
-  pageNumber: any;
-  pagination: any;
-  pageSliceAction: Action[] = []; //pageSliceNews des action
-  searchTermAction = ''; //search term des actions
-  filteredItemsAction: Action[] = [];
-  actionsFiltreParBousre: Action[] = [];
-  dataPageTotal:any;
-
-  currentLink = 'paris';
-
-
-  //filtrage
-  All()
-  {
-    this.filteredItemsAction=this.actionsFiltreParBousre=this.actions;
-    this.pagination=this.dataPageTotal;
-
-      this.actionsFiltreParBousre= this.actionsFiltreParBousre.slice(0,10);
-
-
-  }
- filterActionByLetter(letter:string)
- { console.log('letter',letter)
- console.log('action f',this.actionsFiltreParBourse)
- console.log('action',this.actions)
-
-  this.actionsFiltreParBousre= this.actions.filter((item)=>
-  
-    {console.log('item',item)
-      if (typeof item.nomEntreprise === 'string' && item.nomEntreprise.startsWith(letter))
-       { 
-        return item
-        } 
-        else
-        {    console.log('nulll')
-              return false;
-        }
-    
-  });
-  this.pagination=this.actionsFiltreParBousre.length;
-
-  this.actionsFiltreParBousre= this.actionsFiltreParBousre.slice(0,10);
-
- }
-
-
   alpha = Array(26).fill(0).map((x, i) => String.fromCharCode(i + 65));
-
+  actions: Action[] = [];
   nomBourse: string;
   actionsFiltreParBourse: any[] = [];
   pageSize = 10;
   pageIndex = 0;
   length = 0;
-
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.nomBourse = params['nomBourse'];
-
-
-      
-       console.log('nnnn',this.actions['action']);
-
-      }
-    );
-  
-    
-  /*selectBourse(location: string, pageNumber: any): void {
-    
-    this.actionService.getActions(location, pageNumber).subscribe(data => {
-      this.actions=data.products;
-      this.actionsFiltreParBousre = data.products;
-      this.dataPageTotal=data.pages;
-      this.pagination = data.pages;
-      console.log("actionsFiltreParBourse",this.actionsFiltreParBousre);
-      console.log("pagination",this.pagination)
-      console.log('actions select',this.actions)*/
       if (!this.nomBourse ) {
         this.nomBourse = 'paris';
       }
-
       this.selectBourse(this.nomBourse);
-    }
-  
-
+    });
+  }
   selectBourse(nomBourse: string): void {
     this.actionService.getActionsParBourse(nomBourse).subscribe(data => {
       this.actions = data;
-  
+
       this.actionsFiltreParBourse = this.actions.slice(0, this.pageSize);
-  
+
       this.length = this.actions.length;
     });
   }
-  
-
   navigateToPlace(nomBourse: string): void {
     const route = `/home/${nomBourse}`;
-    this.router.navigate([route]);
-  }
-
-  onPageChange(event): void {
-  this.pageIndex = event.pageIndex;
-  const startIndex = this.pageIndex * this.pageSize;
-  const endIndex = startIndex + this.pageSize;
-  this.actionsFiltreParBourse = this.actions.slice(startIndex, endIndex);
-}
-
-  
-}
+    this.router.navigate([route]);}
+    onPageChange(event): void {
+      this.pageIndex = event.pageIndex;
+      const startIndex = this.pageIndex * this.pageSize;
+      const endIndex = startIndex + this.pageSize;
+      this.actionsFiltreParBourse = this.actions.slice(startIndex, endIndex);
+    }
+    
+    
+    }
